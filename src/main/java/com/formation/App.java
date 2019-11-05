884b22d3f1774b6370746784b8f57d194486b28a
ahcysf58 <youahcene@gmail.com>
2019-11-05 12:31:14 +0100

    initial commit

---
+++ src/main/java/com/formation/App.java
@@ -0,0 +1,13 @@
+package com.formation;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class App {
+
+	public static void main(String[] args) {
+		int a=10;
+		a +=1;
+		System.out.println(a);
+		System.out.println("Hello World !");
+	}
+}
